674ca704681c5c30c5b4c5d36619af06188bd001
anushkac1 <33737038+anushkac1@users.noreply.github.com>
2017-11-16 15:54:57 -0800

    created main js

---
+++ main.js
@@ -0,0 +1,10 @@
+function math()
+  var answer = prompt ("enter 1+1");
+  if (answer ==2){
+    confirm ("you got it. your aswer was"  + 
+    }
+    else {
+       confirm ("you got it wrong")'
+      }
+    }
+ 
